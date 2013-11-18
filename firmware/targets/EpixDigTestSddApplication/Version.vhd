11acd2da549b58ce156e51f10ab6a6a9852b3850
Larry Ruckman <ruckman@slac.stanford.edu>
2013-11-18 00:34:57 +0000

    added FpgaBaseClock register value
    git-svn-id: file:///afs/slac/g/reseng/svn/repos/epix/trunk@3514 0848cbd7-7569-0410-ab05-8cc0a0c67708
    
    Former-commit-id: f4acaadb97329dd8cb36db991a78d9e9da59e517 [formerly 2d92454e5af2f2212a62a77ab4543a4336c6c740]
    Former-commit-id: 0b5a9f9eb75e010b4b1806f661e6fa7fd613ec40

--- firmware/targets/EpixDigTestSddApplication/Version.vhd
+++ firmware/targets/EpixDigTestSddApplication/Version.vhd
@@ -17,6 +17,10 @@ USE ieee.std_logic_1164.ALL;
 package Version is
 
 constant FpgaVersion : std_logic_vector(31 downto 0) := x"E1000001"; -- MAKE_VERSION
+constant FpgaBaseClock : std_logic_vector(31 downto 0) := x"00" & x"200000";  
+-- FPGA base clock (used for calculating various delay units)
+-- Top two nybbles reserved
+-- Bottom 6 nybbles are base clock rate in kHz (binary coded decimal)
 
 end Version;
 
