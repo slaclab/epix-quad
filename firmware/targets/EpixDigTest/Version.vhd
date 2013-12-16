30dae5c3b307e859e049202bb2da6b98dc40c2cc
Kurtis Nishimura <kurtisn@slac.stanford.edu>
2013-12-16 18:27:52 +0000

    New version file and bit files to support SSRL ePix test.
    git-svn-id: file:///afs/slac/g/reseng/svn/repos/epix/trunk@3703 0848cbd7-7569-0410-ab05-8cc0a0c67708
    
    Former-commit-id: 916cad5a3281c0b4bf58b69c2e56da0d6a400645 [formerly 85e13862d817593fd5315d61b80a8de4ce45e525]
    Former-commit-id: c2989792c7ad1b0607b6a4641dad0e79afe1a87e

--- firmware/targets/EpixDigTest/Version.vhd
+++ firmware/targets/EpixDigTest/Version.vhd
@@ -16,7 +16,7 @@ USE ieee.std_logic_1164.ALL;
 
 package Version is
 
-constant FpgaVersion : std_logic_vector(31 downto 0) := x"E0000006"; -- MAKE_VERSION
+constant FpgaVersion : std_logic_vector(31 downto 0) := x"E0000008"; -- MAKE_VERSION
 constant FpgaBaseClock : std_logic_vector(31 downto 0) := x"00" & x"125000";  
 -- FPGA base clock (used for calculating various delay units)
 -- Top two nybbles reserved
@@ -30,5 +30,10 @@ end Version;
 -- 11/05/2013 (0xE0000005): First version that supports "final" readout format.
 -- 11/15/2013 (0xE0000006): Added "BaseClock" register at 0x000010.  Turned off 
 --                          DAC SPI clock when not in use to reduce ADC noise.
+-- 12/03/2013 (0xE0000007): Modified trig_out behavior so something is still 
+--                          sent out when in ADC stream mode.
+-- 12/16/2013 (0xE0000008): Modified readout control for SSRL ePix test.
+--                          Words 1 and 2 of the "TPS" data now come from
+--                          the slow ADC thermistor channels rather than ASICs. 
 -------------------------------------------------------------------------------
 
