ec93be9b36dc89cb385c82ea9b19eccc4e13901b
Maciej Kwiatkowski <mkwiatko@slac.stanford.edu>
2016-03-10 22:14:00 +0000

    git-svn-id: file:///afs/slac/g/reseng/svn/repos/epix/trunk@9647 0848cbd7-7569-0410-ab05-8cc0a0c67708
    Former-commit-id: 0d4020e35ab60445971df117fe2646d61c2d4eff [formerly c186ab785c65752e8b17347eba15c2a325b2a5c6]
    Former-commit-id: c11abebaba1105052fff13871911f665f12a98b1

---
+++ firmware/simulations/CpixLUT/vivado/project_setup.tcl
@@ -0,0 +1,4 @@
+
+set_property STEPS.SYNTH_DESIGN.ARGS.FLATTEN_HIERARCHY none [get_runs synth_1]
+
+set_property top {TB_CpixLUT} [get_filesets sim_1]
---
+++ firmware/simulations/CpixLUT/vivado/sources.tcl
@@ -0,0 +1,1 @@
+set_property top TB_CpixLUT [current_fileset]
