3c57b2d761119ac32569654d7188dbbd31f5488b
Benjamin Reese <bareese@slac.stanford.edu>
2016-11-11 14:57:34 -0800

    Use new ruckus v2

---
+++ firmware/targets/Coulter/ruckus.tcl
@@ -0,0 +1,9 @@
+# Load RUCKUS environment and library
+source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
+
+# Load Source Code
+loadSource -path "$::DIR_PATH/Version.vhd"
+loadSource -dir "$::DIR_PATH/hdl/"
+
+# Load Constraints
+loadConstraints -path "$::DIR_PATH/hdl/Coulter.xdc
