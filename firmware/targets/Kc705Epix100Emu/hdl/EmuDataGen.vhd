5bc292af2ed38463c486132472abc6ee809e00c1
Larry Ruckman <ruckman@slac.stanford.edu>
2017-01-25 22:19:06 +0000

    Adding source code
    git-svn-id: file:///afs/slac/g/reseng/svn/repos/epix/trunk@13481 0848cbd7-7569-0410-ab05-8cc0a0c67708

---
+++ firmware/targets/Kc705Epix100Emu/hdl/EmuDataGen.vhd
@@ -0,0 +1,153 @@
+-------------------------------------------------------------------------------
+-- Title      : 
+-------------------------------------------------------------------------------
+-- File       : EmuDataGen.vhd
+-- Author     : Larry Ruckman  <ruckman@slac.stanford.edu>
+-- Company    : SLAC National Accelerator Laboratory
+-- Created    : 2017-01-25
+-- Last update: 2017-01-25
+-- Platform   : 
+-- Standard   : VHDL'93/02
+-------------------------------------------------------------------------------
+-- Description: 
+-------------------------------------------------------------------------------
+-- This file is part of 'Example Project Firmware'.
+-- It is subject to the license terms in the LICENSE.txt file found in the 
+-- top-level directory of this distribution and at: 
+--    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html. 
+-- No part of 'Example Project Firmware', including this file, 
+-- may be copied, modified, propagated, or distributed except according to 
+-- the terms contained in the LICENSE.txt file.
+-------------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
+use ieee.std_logic_unsigned.all;
+
+use work.StdRtlPkg.all;
+use work.AxiStreamPkg.all;
+use work.SsiPkg.all;
+
+entity EmuDataGen is
+   generic (
+      TPD_G  : time    := 1 ns;
+      LANE_G : natural := 0);
+   port (
+      -- Clock and Reset
+      clk      : in  sl;
+      rst      : in  sl;
+      -- Trigger Interface
+      opCodeEn : in  sl;
+      opCode   : in  slv(7 downto 0);
+      -- Streaming Interface
+      txMaster : out AxiStreamMasterType;
+      txSlave  : in  AxiStreamSlaveType);
+end EmuDataGen;
+
+architecture rtl of EmuDataGen is
+
+   constant MAX_CNT_C    : natural             := 272650;  -- (1090604/4)-1
+   constant AXI_CONFIG_G : AxiStreamConfigType := ssiAxiStreamConfig(4, TKEEP_COMP_C);
+
+   type StateType is (
+      IDLE_S,
+      MOVE_S);
+
+   type RegType is record
+      opCode   : slv(7 downto 0);
+      cnt      : natural range 0 to (MAX_CNT_C+1);
+      txMaster : AxiStreamMasterType;
+      state    : StateType;
+   end record RegType;
+   constant REG_INIT_C : RegType := (
+      opCode   => x"00",
+      cnt      => 0,
+      txMaster => AXI_STREAM_MASTER_INIT_C,
+      state    => IDLE_S);
+
+   signal r   : RegType := REG_INIT_C;
+   signal rin : RegType;
+
+begin
+
+   comb : process (opCode, opCodeEn, r, rst, txSlave) is
+      variable v : RegType;
+      variable i : natural;
+   begin
+      -- Latch the current value
+      v := r;
+
+      -- Reset the flags
+      if txSlave.tReady = '1' then
+         v.txMaster.tValid := '0';
+         v.txMaster.tLast  := '0';
+         v.txMaster.tUser  := (others => '0');
+         v.txMaster.tKeep  := (others => '1');
+      end if;
+
+      -- State Machine
+      case r.state is
+         ----------------------------------------------------------------------
+         when IDLE_S =>
+            -- Check for trigger
+            if (opCodeEn = '1') then
+               -- Save the OP-code value
+               v.opCode := opCode;
+               -- Next state
+               v.state  := MOVE_S;
+            end if;
+         ----------------------------------------------------------------------
+         when MOVE_S =>
+            -- Check if ready to move data
+            if (v.txMaster.tValid = '0') then
+               -- Write the data 
+               v.txMaster.tValid := '1';
+               -- Reset the data bus
+               v.txMaster.tData  := (others => '0');
+               -- Increment the counter
+               v.cnt             := r.cnt + 1;
+               -- Check for first word
+               if (r.cnt = 0) then
+                  -- Set the SOF bit
+                  ssiSetUserSof(AXI_CONFIG_G, v.txMaster, '1');
+                  -- Insert the lane pointer
+                  v.txMaster.tData(7 downto 5) := toSlv(LANE_G, 3);
+               -- Check for second word
+               elsif (r.cnt = 1) then
+                  -- Insert the op-code
+                  v.txMaster.tData(23 downto 16) := r.opCode;
+               -- Check for last word
+               elsif (r.cnt = MAX_CNT_C) then
+                  -- Reset the counter
+                  v.cnt            := 0;
+                  -- Set the EOF bit
+                  v.txMaster.tLast := '1';
+                  -- Next state
+                  v.state          := IDLE_S;
+               end if;
+            end if;
+      ----------------------------------------------------------------------
+      end case;
+
+      -- Reset
+      if (rst = '1') then
+         v := REG_INIT_C;
+      end if;
+
+      -- Register the variable for next clock cycle
+      rin <= v;
+
+      -- Outputs              
+      txMaster <= r.txMaster;
+
+   end process comb;
+
+   seq : process (clk) is
+   begin
+      if rising_edge(clk) then
+         r <= rin after TPD_G;
+      end if;
+   end process seq;
+
+end rtl;
---
+++ firmware/targets/Kc705Epix100Emu/hdl/PgpWrapper.vhd
@@ -0,0 +1,239 @@
+-------------------------------------------------------------------------------
+-- Title      : 
+-------------------------------------------------------------------------------
+-- File       : PgpWrapper.vhd
+-- Author     : Larry Ruckman  <ruckman@slac.stanford.edu>
+-- Company    : SLAC National Accelerator Laboratory
+-- Created    : 2017-01-25
+-- Last update: 2017-01-25
+-- Platform   : 
+-- Standard   : VHDL'93/02
+-------------------------------------------------------------------------------
+-- Description: 
+-------------------------------------------------------------------------------
+-- This file is part of 'Example Project Firmware'.
+-- It is subject to the license terms in the LICENSE.txt file found in the 
+-- top-level directory of this distribution and at: 
+--    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html. 
+-- No part of 'Example Project Firmware', including this file, 
+-- may be copied, modified, propagated, or distributed except according to 
+-- the terms contained in the LICENSE.txt file.
+-------------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+use work.StdRtlPkg.all;
+use work.Pgp2bPkg.all;
+use work.AxiLitePkg.all;
+use work.AxiStreamPkg.all;
+use work.SsiPkg.all;
+use work.SsiCmdMasterPkg.all;
+use work.EpixPkgGen2.all;
+
+entity PgpWrapper is
+   generic (
+      TPD_G : time := 1 ns);
+   port (
+      -- Clocks and Reset
+      refClk   : in  sl;
+      clk      : in  sl;
+      rst      : in  sl;
+      -- Non VC TX Signals
+      pgpTxIn  : in  Pgp2bTxInType;
+      pgpTxOut : out Pgp2bTxOutType;
+      -- Non VC RX Signals
+      pgpRxIn  : in  Pgp2bRxInType;
+      pgpRxOut : out Pgp2bRxOutType;
+      -- Streaming Interface
+      txMaster : in  AxiStreamMasterType;
+      txSlave  : out AxiStreamSlaveType;
+      -- GT Pins
+      gtTxP    : out sl;
+      gtTxN    : out sl;
+      gtRxP    : in  sl;
+      gtRxN    : in  sl);
+end PgpWrapper;
+
+architecture mapping of PgpWrapper is
+
+   signal pgpTxMasters : AxiStreamMasterArray(3 downto 0) := (others => AXI_STREAM_MASTER_INIT_C);
+   signal pgpTxSlaves  : AxiStreamSlaveArray(3 downto 0)  := (others => AXI_STREAM_SLAVE_FORCE_C);
+
+   signal pgpRxMasters : AxiStreamMasterArray(3 downto 0) := (others => AXI_STREAM_MASTER_INIT_C);
+   signal pgpRxCtrl    : AxiStreamCtrlArray(3 downto 0)   := (others => AXI_STREAM_CTRL_UNUSED_C);
+
+   signal axilReadMaster  : AxiLiteReadMasterType;
+   signal axilReadSlave   : AxiLiteReadSlaveType;
+   signal axilWriteMaster : AxiLiteWriteMasterType;
+   signal axilWriteSlave  : AxiLiteWriteSlaveType;
+
+   signal axilReadMasters  : AxiLiteReadMasterArray(NUM_AXI_MASTER_SLOTS_C-1 downto 0);
+   signal axilReadSlaves   : AxiLiteReadSlaveArray(NUM_AXI_MASTER_SLOTS_C-1 downto 0);
+   signal axilWriteMasters : AxiLiteWriteMasterArray(NUM_AXI_MASTER_SLOTS_C-1 downto 0);
+   signal axilWriteSlaves  : AxiLiteWriteSlaveArray(NUM_AXI_MASTER_SLOTS_C-1 downto 0);
+
+
+begin
+
+   U_Pgp2bGtx7VarLat : entity work.Pgp2bGtx7VarLat
+      generic map (
+         TPD_G             => TPD_G,
+         -- CPLL Configurations
+         TX_PLL_G          => "CPLL",
+         RX_PLL_G          => "CPLL",
+         CPLL_REFCLK_SEL_G => "001",
+         CPLL_FBDIV_G      => 5,
+         CPLL_FBDIV_45_G   => 5,
+         CPLL_REFCLK_DIV_G => 1,
+         -- MGT Configurations
+         RXOUT_DIV_G       => 2,
+         TXOUT_DIV_G       => 2,
+         RX_CLK25_DIV_G    => 5,
+         TX_CLK25_DIV_G    => 5,
+         RX_OS_CFG_G       => "0000010000000",
+         RXCDR_CFG_G       => x"03000023ff40200020",
+         RXDFEXYDEN_G      => '1',
+         RX_DFE_KL_CFG2_G  => x"301148AC",
+         -- VC Configuration
+         VC_INTERLEAVE_G   => 0,
+         PAYLOAD_CNT_TOP_G => 7,
+         NUM_VC_EN_G       => 4,
+         AXI_ERROR_RESP_G  => AXI_RESP_DECERR_C,
+         TX_ENABLE_G       => true,
+         RX_ENABLE_G       => true)
+      port map (
+         -- GT Clocking
+         stableClk        => clk,
+         gtCPllRefClk     => refClk,
+         gtCPllLock       => open,
+         gtQPllRefClk     => '0',
+         gtQPllClk        => '0',
+         gtQPllLock       => '1',
+         gtQPllRefClkLost => '0',
+         gtQPllReset      => open,
+         -- GT Serial IO
+         gtTxP            => gtTxP,
+         gtTxN            => gtTxN,
+         gtRxP            => gtRxP,
+         gtRxN            => gtRxN,
+         -- Tx Clocking
+         pgpTxReset       => rst,
+         pgpTxRecClk      => open,
+         pgpTxClk         => clk,
+         pgpTxMmcmReset   => open,
+         pgpTxMmcmLocked  => '1',
+         -- Rx clocking
+         pgpRxReset       => rst,
+         pgpRxRecClk      => open,
+         pgpRxClk         => clk,
+         pgpRxMmcmReset   => open,
+         pgpRxMmcmLocked  => '1',
+         -- Non VC TX Signals
+         pgpTxIn          => pgpTxIn,
+         pgpTxOut         => pgpTxOut,
+         -- Non VC RX Signals
+         pgpRxIn          => pgpRxIn,
+         pgpRxOut         => pgpRxOut,
+         -- Frame TX Interface
+         pgpTxMasters     => pgpTxMasters,
+         pgpTxSlaves      => pgpTxSlaves,
+         -- Frame RX Interface
+         pgpRxMasters     => pgpRxMasters,
+         pgpRxCtrl        => pgpRxCtrl,
+         -- Debug Interface 
+         txPreCursor      => (others => '0'),
+         txPostCursor     => (others => '0'),
+         txDiffCtrl       => "1111");
+
+   -- Lane 0, VC0 TX, streaming data out 
+   U_Vc0SsiTxFifo : entity work.AxiStreamFifo
+      generic map (
+         TPD_G               => TPD_G,
+         --EN_FRAME_FILTER_G   => true,
+         CASCADE_SIZE_G      => 1,
+         BRAM_EN_G           => true,
+         USE_BUILT_IN_G      => false,
+         GEN_SYNC_FIFO_G     => true,
+         FIFO_ADDR_WIDTH_G   => 14,
+         FIFO_FIXED_THRESH_G => true,
+         FIFO_PAUSE_THRESH_G => 128,
+         SLAVE_AXI_CONFIG_G  => ssiAxiStreamConfig(4, TKEEP_COMP_C),
+         MASTER_AXI_CONFIG_G => SSI_PGP2B_CONFIG_C)
+      port map (
+         -- Slave Port
+         sAxisClk    => clk,
+         sAxisRst    => rst,
+         sAxisMaster => txMaster,
+         sAxisSlave  => txSlave,
+         -- Master Port
+         mAxisClk    => clk,
+         mAxisRst    => rst,
+         mAxisMaster => pgpTxMasters(0),
+         mAxisSlave  => pgpTxSlaves(0));
+
+   -- Lane 0, VC1 RX/TX, Register access control        
+   U_Vc1AxiMasterRegisters : entity work.SsiAxiLiteMaster
+      generic map (
+         TPD_G               => TPD_G,
+         GEN_SYNC_FIFO_G     => true,
+         USE_BUILT_IN_G      => false,
+         EN_32BIT_ADDR_G     => true,
+         AXI_STREAM_CONFIG_G => SSI_PGP2B_CONFIG_C)
+      port map (
+         -- Streaming Slave (Rx) Interface (sAxisClk domain) 
+         sAxisClk            => clk,
+         sAxisRst            => rst,
+         sAxisMaster         => pgpRxMasters(1),
+         sAxisSlave          => open,
+         sAxisCtrl           => pgpRxCtrl(1),
+         -- Streaming Master (Tx) Data Interface (mAxisClk domain)
+         mAxisClk            => clk,
+         mAxisRst            => rst,
+         mAxisMaster         => pgpTxMasters(1),
+         mAxisSlave          => pgpTxSlaves(1),
+         -- AXI Lite Bus (axiLiteClk domain)
+         axiLiteClk          => clk,
+         axiLiteRst          => rst,
+         mAxiLiteWriteMaster => axilWriteMaster,
+         mAxiLiteWriteSlave  => axilWriteSlave,
+         mAxiLiteReadMaster  => axilReadMaster,
+         mAxiLiteReadSlave   => axilReadSlave);
+
+   U_XBAR : entity work.AxiLiteCrossbar
+      generic map (
+         TPD_G              => TPD_G,
+         NUM_SLAVE_SLOTS_G  => 1,
+         NUM_MASTER_SLOTS_G => NUM_AXI_MASTER_SLOTS_C,
+         MASTERS_CONFIG_G   => AXI_CROSSBAR_MASTERS_CONFIG_C)
+      port map (
+         sAxiWriteMasters(0) => axilWriteMaster,
+         sAxiWriteSlaves(0)  => axilWriteSlave,
+         sAxiReadMasters(0)  => axilReadMaster,
+         sAxiReadSlaves(0)   => axilReadSlave,
+         mAxiWriteMasters    => axilWriteMasters,
+         mAxiWriteSlaves     => axilWriteSlaves,
+         mAxiReadMasters     => axilReadMasters,
+         mAxiReadSlaves      => axilReadSlaves,
+         axiClk              => clk,
+         axiClkRst           => rst);
+
+   GEN_VEC :
+   for i in (NUM_AXI_MASTER_SLOTS_C-1) downto 0 generate
+      U_RAM : entity work.AxiDualPortRam
+         generic map (
+            TPD_G        => TPD_G,
+            BRAM_EN_G    => true,
+            REG_EN_G     => true,
+            ADDR_WIDTH_G => 12,
+            DATA_WIDTH_G => 32)
+         port map (
+            axiClk         => clk,
+            axiRst         => rst,
+            axiReadMaster  => axilReadMasters(i),
+            axiReadSlave   => axilReadSlaves(i),
+            axiWriteMaster => axilWriteMasters(i),
+            axiWriteSlave  => axilWriteSlaves(i));
+   end generate GEN_VEC;
+
+end mapping;
