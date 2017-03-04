e079706b3345e1810ae39cb59f6c2e4bb3aa7d30
Benjamin Reese <bareese@slac.stanford.edu>
2017-03-03 16:43:19 -0800

    Add whitespace

--- software/Coulter/python/coulter/_coulter.py
+++ software/Coulter/python/coulter/_coulter.py
@@ -198,6 +198,8 @@ class ELine100Config(pr.Device):
             if n != "EnaAnalogMonitor":
                 v.beforeReadCmd = self.ReadAsic
                 v.afterWriteCmd = self.WriteAsic
+
+                
 class AdcStreamFilter(pr.Device):
     def __init__(self, **kwargs):
         super(self.__class__, self).__init__(**kwargs)
