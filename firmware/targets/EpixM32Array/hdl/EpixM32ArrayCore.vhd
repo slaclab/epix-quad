721fc1c862e239574b0a9ef25adeec670a75f6b8
mkwiatko <mwkwiatko@gmail.com>
2017-08-31 15:49:03 -0700

    connected signal to pseudoscope

--- firmware/targets/EpixM32Array/hdl/EpixM32ArrayCore.vhd
+++ firmware/targets/EpixM32Array/hdl/EpixM32ArrayCore.vhd
@@ -919,6 +919,7 @@ begin
       sysClkRst      => axiRst,
       adcData        => adcData,
       adcValid       => adcValid,
+      adcMarker      => (others=>'iAsicSample'),
       arm            => acqStart,
       acqStart       => acqStart,
       asicAcq        => acqStart,
