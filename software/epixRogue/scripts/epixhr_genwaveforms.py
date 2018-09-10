eacc4dfad124d6542ae7c44e247f3e8b2f591601
Faisal Abu-Nimeh <abunimeh@slac.stanford.edu>
2018-09-10 12:31:57 -0700

    parameterize amplitude

--- software/epixRogue/scripts/epixhr_genwaveforms.py
+++ software/epixRogue/scripts/epixhr_genwaveforms.py
@@ -5,20 +5,28 @@ import matplotlib.pyplot as plt
 import numpy as np
 import sys
 
+# constants
 dac_resol = 2**16-1
 dac_mem = 1024
+shrinkfactor = 0.30 / 2
+shrink = shrinkfactor * dac_resol  # shrink 15% from top and %15 from bottom = 30%
+# shrink = 0
+# shrinkfactor = 0
+
 
 # incr ramp
-ramp = np.linspace(0, dac_resol, dac_mem, dtype=int)
+ramp = np.linspace(0+shrink, dac_resol-shrink, dac_mem, dtype=int)
 np.savetxt("ramp.csv", ramp, fmt="%d")
 
 # decr ramp
-rampf = np.linspace(dac_resol, 0, dac_mem, dtype=int)
+rampf = np.linspace(dac_resol-shrink, 0+shrink, dac_mem, dtype=int)
 np.savetxt("rampf.csv", rampf, fmt="%d")
 
 # sine
 angle = 2*np.pi*np.linspace(0, 1, dac_mem)
-sine = dac_resol/2*np.sin(angle)+dac_resol/2
+offset = dac_resol/2
+amp = (1-2*shrinkfactor)*dac_resol/2  # 70% of full scale
+sine = amp*np.sin(angle)+offset
 np.savetxt("sine.csv", sine, fmt="%d")
 
 if len(sys.argv) > 1 and sys.argv[1] == '-p':
