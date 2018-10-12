bb803f5d8eb059c2aeb9a8702cc5c7c174a6d715
mkwiatko <mwkwiatko@gmail.com>
2018-10-11 18:35:15 -0700

    added linked variables

--- software/epixRogue/python/ePixQuad/EpixQuadMonitoring.py
+++ software/epixRogue/python/ePixQuad/EpixQuadMonitoring.py
@@ -25,6 +25,23 @@ class EpixQuadMonitor(pr.Device):
       """Create the configuration device for Monitoring Core data readout"""
       super().__init__(description='Temperature Sensors Registers', **kwargs)
       
+      def getShtHum(var):
+         x = var.dependencies[0].value()
+         return x / 65535.0 * 100.0
+      
+      def getShtTemp(var):
+         x = var.dependencies[0].value()
+         return x / 65535.0 * 175.0 - 45.0
+      
+      def getNctTemp(var):
+         x = var.dependencies[0].value()
+         y = var.dependencies[1].value()
+         return x * 1.0 + (y >> 6) * 0.25
+      
+      def getNctTempLoc(var):
+         x = var.dependencies[0].value()
+         return x * 1.0 
+         
       # Creation. memBase is either the register bus server (srp, rce mapped memory, etc) or the device which
       # contains this object. In most cases the parent and memBase are the same but they can be 
       # different in more complex bus structures. They will also be different for the top most node.
@@ -41,12 +58,44 @@ class EpixQuadMonitor(pr.Device):
       self.add((pr.RemoteVariable(name='MonitorStrEn',   description='Monitor Stream Enabled',      offset=0x00000004, bitSize=1,  bitOffset=0,  base=pr.Bool, mode='RO')))
       self.add((pr.RemoteVariable(name='TrigPrescaler',  description='Monitor Triggger Prescaler',  offset=0x00000008, bitSize=16, bitOffset=0,  base=pr.UInt, mode='RW')))
       self.add((pr.RemoteVariable(name='ShtError',       description='SHT31 Humidity Sensor Error', offset=0x0000000C, bitSize=16, bitOffset=0,  base=pr.UInt, mode='RO')))
-      self.add((pr.RemoteVariable(name='ShtHum',         description='SHT31 Humidity Value',        offset=0x00000010, bitSize=16, bitOffset=0,  base=pr.UInt, mode='RO')))
-      self.add((pr.RemoteVariable(name='ShtTemp',        description='SHT31 Temperature Value',     offset=0x00000014, bitSize=16, bitOffset=0,  base=pr.UInt, mode='RO')))
+      self.add((pr.RemoteVariable(name='ShtHumRaw',      description='SHT31 Humidity Value',        offset=0x00000010, bitSize=16, bitOffset=0,  base=pr.UInt, mode='RO')))
+      self.add(pr.LinkVariable(
+         name         = 'ShtHum', 
+         mode         = 'RO', 
+         units        = '%',
+         linkedGet    = getShtHum,
+         disp         = '{:1.2f}',
+         dependencies = [self.ShtHumRaw],
+      )) 
+      self.add((pr.RemoteVariable(name='ShtTempRaw',     description='SHT31 Temperature Value',     offset=0x00000014, bitSize=16, bitOffset=0,  base=pr.UInt, mode='RO')))
+      self.add(pr.LinkVariable(
+         name         = 'ShtTemp', 
+         mode         = 'RO', 
+         units        = 'deg C',
+         linkedGet    = getShtTemp,
+         disp         = '{:1.2f}',
+         dependencies = [self.ShtTempRaw],
+      )) 
       self.add((pr.RemoteVariable(name='NctError',       description='NCT218 Temp. Sensor Error',   offset=0x00000018, bitSize=16, bitOffset=0,  base=pr.UInt, mode='RO')))
-      self.add((pr.RemoteVariable(name='NctLocTemp',     description='NCT218 Local Temp.',          offset=0x0000001C, bitSize=8,  bitOffset=0,  base=pr.UInt, mode='RO')))
-      self.add((pr.RemoteVariable(name='NctRemTempL',    description='NCT218 Remote Temp. L Byte',  offset=0x00000020, bitSize=8,  bitOffset=0,  base=pr.UInt, mode='RO')))
-      self.add((pr.RemoteVariable(name='NctRemTempH',    description='NCT218 Remote Temp. H Byte',  offset=0x00000024, bitSize=8,  bitOffset=0,  base=pr.UInt, mode='RO')))
+      self.add((pr.RemoteVariable(name='NctLocTempRaw',  description='NCT218 Local Temp.',          offset=0x0000001C, bitSize=8,  bitOffset=0,  base=pr.UInt, mode='RO')))
+      self.add(pr.LinkVariable(
+         name         = 'NctLocTemp', 
+         mode         = 'RO', 
+         units        = 'deg C',
+         linkedGet    = getNctTempLoc,
+         disp         = '{:1.2f}',
+         dependencies = [self.NctLocTempRaw],
+      )) 
+      self.add((pr.RemoteVariable(name='NctRemTempLRaw', description='NCT218 Remote Temp. L Byte',  offset=0x00000020, bitSize=8,  bitOffset=0,  base=pr.UInt, mode='RO')))
+      self.add((pr.RemoteVariable(name='NctRemTempHRaw', description='NCT218 Remote Temp. H Byte',  offset=0x00000024, bitSize=8,  bitOffset=0,  base=pr.UInt, mode='RO')))
+      self.add(pr.LinkVariable(
+         name         = 'NctRemTemp', 
+         mode         = 'RO', 
+         units        = 'deg C',
+         linkedGet    = getNctTemp,
+         disp         = '{:1.2f}',
+         dependencies = [self.NctRemTempHRaw, self.NctRemTempLRaw],
+      )) 
       
       #####################################
       # Create commands
