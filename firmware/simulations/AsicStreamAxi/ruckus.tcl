d47f802898ba9f7c68231789ce52a5b77d0035ce
mkwiatko <mwkwiatko@gmail.com>
2019-01-18 11:06:37 -0800

    remove obsolete simulation projects

--- firmware/simulations/AsicStreamAxi/ruckus.tcl
+++
@@ -1,8 +0,0 @@
-# Load RUCKUS environment and library
-source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
-
-# Load library modules
-loadRuckusTcl $::env(PROJ_DIR)/../../submodules/surf
-loadRuckusTcl $::env(PROJ_DIR)/../../common/CpixTixelCommon
-
-set_property top {AsicStreamAxiTb} [get_filesets sim_1]
--- firmware/simulations/CpixLUT/ruckus.tcl
+++
@@ -1,7 +0,0 @@
-# Load RUCKUS environment and library
-source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
-
-# Load library modules
-loadRuckusTcl $::env(PROJ_DIR)/../../submodules/surf
-loadRuckusTcl $::env(PROJ_DIR)/../../common/EpixCommonGen2
-loadRuckusTcl $::env(PROJ_DIR)/../../common/CpixTixelCommon
