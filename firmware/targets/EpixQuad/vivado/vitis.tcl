08d33b85ce10e86d9a807b1fef816ae9dd44d716
Larry Ruckman <ruckman@slac.stanford.edu>
2020-01-24 09:38:22 -0800

    adding target vivado/vitis.tcl

---
+++ firmware/targets/EpixQuad/vivado/vitis.tcl
@@ -0,0 +1,11 @@
+##############################################################################
+## This file is part of 'EPIX Development Firmware'.
+## It is subject to the license terms in the LICENSE.txt file found in the 
+## top-level directory of this distribution and at: 
+##    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html. 
+## No part of 'EPIX Development Firmware', including this file, 
+## may be copied, modified, propagated, or distributed except according to 
+## the terms contained in the LICENSE.txt file.
+##############################################################################
+
+source $::env(PROJ_DIR)/../../common/EpixQuadCommmon/vivado/vivado_vitis.tcl
