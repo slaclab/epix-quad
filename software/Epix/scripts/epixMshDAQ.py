74afbdb7f7f3f34f28f8aa6f14d9bf5c2bcada6f
mkwiatko <mwkwiatko@gmail.com>
2020-04-10 16:58:32 -0700

    adding new ePixM DAQ, removing old

--- software/Epix/scripts/epixMshDAQ.py
+++ software/Epix/scripts/epixMshDAQ.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 #-----------------------------------------------------------------------------
-# Title      : ePixM32 array prototype board instance
+# Title      : ePixMsh board instance
 #-----------------------------------------------------------------------------
-# File       : epixM32Array.py
-# Author     : Maciej Kwiatkowski, mkwiatko@slac.stanford.edu
+# File       : epixMshDAQ.py evolved from evalBoard.py
+# Author     : Ryan Herbst, rherbst@slac.stanford.edu
+# Modified by: Dionisio Doering
 # Created    : 2016-09-29
 # Last update: 2017-02-01
 #-----------------------------------------------------------------------------
 # Description:
-# Rogue interface to ePix 10ka board
+# Rogue interface to ePixMsh board
 #-----------------------------------------------------------------------------
 # This file is part of the rogue_example software. It is subject to 
 # the license terms in the LICENSE.txt file found in the top-level directory 
@@ -19,8 +20,10 @@
 # contained in the LICENSE.txt file.
 #-----------------------------------------------------------------------------
 import rogue.hardware.pgp
+import pyrogue as pr
 import pyrogue.utilities.prbs
 import pyrogue.utilities.fileio
+import pyrogue.interfaces.simulation
 import pyrogue.gui
 import surf
 import surf.axi
@@ -100,10 +103,17 @@ parser.add_argument(
     help     = "Print debug info",
 )  
 
+parser.add_argument(
+    "--simulation", 
+    type     = argBool,
+    required = False,
+    default  = False,
+    help     = "Connect to VCS simulation",
+)  
+
 
 # Get the arguments
 args = parser.parse_args()
-
 
 #############################################
 # Define if the GUI is started (1 starts it)
@@ -116,13 +126,18 @@ PRINT_VERBOSE = args.verbose
 
 
 # Create the PGP interfaces for ePix camera
-pgpVc0 = rogue.hardware.pgp.PgpCard(args.pgp,0,0) # Data & cmds
-pgpVc1 = rogue.hardware.pgp.PgpCard(args.pgp,0,1) # Registers for ePix board
-pgpVc2 = rogue.hardware.pgp.PgpCard(args.pgp,0,2) # PseudoScope
-pgpVc3 = rogue.hardware.pgp.PgpCard(args.pgp,0,3) # Monitoring (Slow ADC)
-
-print("")
-print("PGP Card Version: %x" % (pgpVc0.getInfo().version))
+if args.simulation:
+   pgpVc1 = rogue.interfaces.stream.TcpClient('localhost',8000)
+   pgpVc0 = rogue.interfaces.stream.TcpClient('localhost',8002)
+   pgpVc2 = rogue.interfaces.stream.TcpClient('localhost',8004)
+   pgpVc3 = rogue.interfaces.stream.TcpClient('localhost',8006)
+else:
+   pgpVc1 = rogue.hardware.pgp.PgpCard(args.pgp,0,0) # Data & cmds
+   pgpVc0 = rogue.hardware.pgp.PgpCard(args.pgp,0,1) # Registers for ePix board
+   pgpVc2 = rogue.hardware.pgp.PgpCard(args.pgp,0,2) # PseudoScope
+   pgpVc3 = rogue.hardware.pgp.PgpCard(args.pgp,0,3) # Monitoring (Slow ADC)
+   print("")
+   print("PGP Card Version: %x" % (pgpVc0.getInfo().version))
 
 
 # Add data stream to file as channel 1
@@ -140,13 +155,54 @@ pyrogue.streamConnect(cmd, pgpVc0)
 srp = rogue.protocols.srp.SrpV0()
 pyrogue.streamConnectBiDir(pgpVc1,srp)
 
+# Add configuration stream to file as channel 0
+# Removed to reduce amount of data going to file
+#pyrogue.streamConnect(ePixBoard,dataWriter.getChannel(0x0))
+
+## Add microblaze console stream to file as channel 2
+#pyrogue.streamConnect(pgpVc3,dataWriter.getChannel(0x2))
+
+# PRBS Receiver as secdonary receiver for VC1
+#prbsRx = pyrogue.utilities.prbs.PrbsRx('prbsRx')
+#pyrogue.streamTap(pgpVc1,prbsRx)
+#ePixBoard.add(prbsRx)
+
+# Microblaze console monitor add secondary tap
+#mbcon = MbDebug()
+#pyrogue.streamTap(pgpVc3,mbcon)
+
+#br = testBridge.Bridge()
+#br._setSlave(srp)
+
+#ePixBoard.add(surf.SsiPrbsTx.create(memBase=srp1,offset=0x00000000*4))
+
+# Create epics node
+#epics = pyrogue.epics.EpicsCaServer('rogueTest',ePixBoard)
+#epics.start()
+
+
+#############################################
+# Microblaze console printout
+#############################################
+class MbDebug(rogue.interfaces.stream.Slave):
+
+    def __init__(self):
+        rogue.interfaces.stream.Slave.__init__(self)
+        self.enable = False
+
+    def _acceptFrame(self,frame):
+        if self.enable:
+            p = bytearray(frame.getPayload())
+            frame.read(p,0)
+            print('-------- Microblaze Console --------')
+            print(p.decode('utf-8'))
 
 #######################################
 # Custom run control
 #######################################
 class MyRunControl(pyrogue.RunControl):
     def __init__(self,name):
-        pyrogue.RunControl.__init__(self,name, description='Run Controller ePixM32Array',  rates={1:'1 Hz', 2:'2 Hz', 4:'4 Hz', 8:'8 Hz', 10:'10 Hz', 30:'30 Hz', 60:'60 Hz', 120:'120 Hz'})
+        pyrogue.RunControl.__init__(self,name, description='Run Controller ePixMsh',  rates={1:'1 Hz', 2:'2 Hz', 4:'4 Hz', 8:'8 Hz', 10:'10 Hz', 30:'30 Hz', 60:'60 Hz', 120:'120 Hz'})
         self._thread = None
 
     def _setRunState(self,dev,var,value,changed):
@@ -181,7 +237,7 @@ class MyRunControl(pyrogue.RunControl):
 ##############################
 class EpixBoard(pyrogue.Root):
     def __init__(self, guiTop, cmd, dataWriter, srp, **kwargs):
-        super().__init__(name = 'ePixBoard',description = 'ePixM32Array Board', **kwargs)
+        super().__init__(name = 'ePixBoard',description = 'ePixMsh Board', **kwargs)
         #self.add(MyRunControl('runControl'))
         self.add(dataWriter)
         self.guiTop = guiTop
@@ -191,12 +247,22 @@ class EpixBoard(pyrogue.Root):
             cmd.sendCmd(0, 0)
 
         # Add Devices
-        self.add(fpga.EpixM32Array(name='EpixM32Array', offset=0, memBase=srp, hidden=False, enabled=True))
-        self.add(pyrogue.RunControl(name = 'runControl', description='Run Controller ePixM32Array', cmd=self.Trigger, rates={1:'1 Hz', 2:'2 Hz', 4:'4 Hz', 8:'8 Hz', 10:'10 Hz', 30:'30 Hz', 60:'60 Hz', 120:'120 Hz'}))
+        self.add(fpga.EpixMsh(name='EpixMsh', offset=0, memBase=srp, hidden=False, enabled=True))
+        self.add(pyrogue.RunControl(name = 'runControl', description='Run Controller ePixMsh', cmd=self.Trigger, rates={1:'1 Hz', 2:'2 Hz', 4:'4 Hz', 8:'8 Hz', 10:'10 Hz', 30:'30 Hz', 60:'60 Hz', 120:'120 Hz'}))
         
 
         
 
+
+# debug
+#mbcon = MbDebug()
+#pyrogue.streamTap(pgpVc0,mbcon)
+
+#mbcon1 = MbDebug()
+#pyrogue.streamTap(pgpVc1,mbcon)
+
+#mbcon2 = MbDebug()
+#pyrogue.streamTap(pgpVc3,mbcon)
 
 if (PRINT_VERBOSE): dbgData = rogue.interfaces.stream.Slave()
 if (PRINT_VERBOSE): dbgData.setDebug(60, "DATA[{}]".format(0))
@@ -205,7 +271,7 @@ if (PRINT_VERBOSE): pyrogue.streamTap(pgpVc0, dbgData)
 
 # Create GUI
 appTop = QApplication(sys.argv)
-guiTop = pyrogue.gui.GuiTop(group = 'ePixM32Array')
+guiTop = pyrogue.gui.GuiTop(group = 'ePixMshGui')
 ePixBoard = EpixBoard(guiTop, cmd, dataWriter, srp)
 ePixBoard.start(
    pollEn   = args.pollEn,
@@ -217,13 +283,15 @@ guiTop.resize(1000,800)
 
 # Viewer gui
 if START_VIEWER:
-   gui = vi.Window(cameraType = 'ePixM32Array')
+   gui = vi.Window(cameraType = 'ePix10ka')
    gui.eventReader.frameIndex = 0
    #gui.eventReaderImage.VIEW_DATA_CHANNEL_ID = 0
    gui.setReadDelay(0)
    pyrogue.streamTap(pgpVc0, gui.eventReader) 
    pyrogue.streamTap(pgpVc2, gui.eventReaderScope)# PseudoScope
    pyrogue.streamTap(pgpVc3, gui.eventReaderMonitoring) # Slow Monitoring
+
+
 
 # Run gui
 if (START_GUI):
@@ -232,7 +300,7 @@ if (START_GUI):
 # Close window and stop polling
 def stop():
     mNode.stop()
+#    epics.stop()
     ePixBoard.stop()
     exit()
-
 
