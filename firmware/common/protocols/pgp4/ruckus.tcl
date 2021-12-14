73d78e1750875b135cf1389708a799af1d258263
Dan Sciortino <disco@slac.stanford.edu>
2021-12-14 15:54:47 -0800

    updating ruckus in protocol folder

--- firmware/common/protocols/pgp4/ruckus.tcl
+++ firmware/common/protocols/pgp4/ruckus.tcl
@@ -6,7 +6,7 @@ loadSource -dir "$::DIR_PATH/rtl"
 
 # Check if required variables exist
 if { [info exists ::env(INCLUDE_PGP4_10G)] != 1 && [info exists ::env(INCLUDE_PGP4_6G)] != 1} {
-   puts "\n\nERROR: The PGP3 link speed (INCLUDE_PGP3_10G or INCLUDE_PGP3_6G or INCLUDE_PGP3_3G) is not defined in $::env(PROJ_DIR)/Makefile\n\n"; exit -1
+   puts "\n\nERROR: The PGP4 link speed (INCLUDE_PGP4_10G or INCLUDE_PGP4_6G) is not defined in $::env(PROJ_DIR)/Makefile\n\n"; exit -1
 }
 
 if { [info exists ::env(INCLUDE_PGP4_10G)] != 1 || $::env(INCLUDE_PGP4_10G) == 0 } {
