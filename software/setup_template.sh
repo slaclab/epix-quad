bd962b0f9463a4c1cfd464c9067b040bdf2edc3b
My Name <myemail@slac.stanford.edu>
2025-03-24 01:59:28 -0700

    update setup and yml

--- software/setup_template.sh
+++ software/setup_template.sh
@@ -2,8 +2,8 @@
 ##################################
 # Setup environment
 ##################################
-source /afs/slac/g/reseng/rogue/anaconda_new_a/anaconda3/etc/profile.d/conda.sh
-#source /afs/slac.stanford.edu/g/reseng/vol31/anaconda/anaconda3/etc/profile.d/conda.sh
+#source /afs/slac/g/reseng/rogue/anaconda_new_a/anaconda3/etc/profile.d/conda.sh
+source /afs/slac.stanford.edu/g/reseng/vol31/anaconda/anaconda3/etc/profile.d/conda.sh
 
 ##################################
 # Activate Rogue conda Environment
